831910599efb4a4182daeadcfa087235c2bc41a8
hieupn <phamnhathieu_t62@hus.edu.vn>
2021-05-05 10:25:14 +0700

    add new function api crud

--- demoDjango/company/views.py
+++ demoDjango/company/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 from rest_framework import status
 from .models import Company
 from .serializers import CompanySerializer
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 @api_view(['GET'])
 def testapi(request):
@@ -54,3 +56,30 @@ def companyDelete(request, pk):
 def view_company(request):
     companies = Company.objects.all()
     return render(request, 'company/list-company.html', {'companies': companies})
+
+@csrf_exempt
+def companyApi(request, id=0):
+    if request.method == 'GET':
+        companies = Company.objects.all()
+        companies_serializer = CompanySerializer(companies, many=True)
+        return JsonResponse(companies_serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CompanySerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse("Added Successfully!", safe=False)
+        return JsonResponse("Fail to add!", safe=False)
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        company = Company.objects.get(id = data['id'])
+        serializer = CompanySerializer(instance=company, data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse("Update successfully!", safe=False)
+        return JsonResponse("Fail to update!", safe=False)
+    elif request.method == "DELETE":
+        company = Company.objects.get(id = id)
+        company.delete()
+        return JsonResponse("Delete successfully!", safe=False)
+
--- demoDjango/demoDjango/urls.py
+++ demoDjango/demoDjango/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('company/', include('company.urls'))
+    path('company/', include('company.urls')),
+    path(r'^', include('company.urls'))
 ]
